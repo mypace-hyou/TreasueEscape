6ed6ac402f697c65d0ab25e3b0ccdf5d399dd687
mypace-hyou <hyou290101@gmail.com>
2020-12-10 03:27:47 +0900

    ブロックを任意の範囲内に敷き詰める。一番外のブロックを赤色にした

--- Assets/TestFolder/test.cs
+++ Assets/TestFolder/test.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     public int zPos = 0;
+    public int xPos = 0;
     public GameObject cube;
     
-    public 
-    // Start is called before the first frame update
     void Start()
     {
+        int zPos = 0;
+        int xPos = 0;
+
         // Z位置を+1しながらマップを生成していく
-        for (; zPos < 10; zPos++)
+        for (; zPos < 15; zPos++)
         {
-            cube.transform.position = new Vector3(0, 0, zPos);
+            // Z位置が更新されたら、まず1つマップチップを置く
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(xPos, 0, zPos);
+
+            // 現在のマップ生成のX位置から移動できるX方向の範囲を決定
+            int randomAddTips_Min = -3 - xPos;
+            int randomAddTips_Max = 3 - xPos;
+
+            // 乱数を用意して、その値分X方向に移動する。0が出たら曲がらず直進する
+            int randomAddTips = Random.Range(randomAddTips_Min, randomAddTips_Max + 1);
+            // 乱数が0以上なら右に曲がり、乱数分進む
+            if (randomAddTips > 0)
+            {
+                for (; randomAddTips > 0; randomAddTips--)
+                {
+                    xPos++;
+                    cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(xPos, 0, zPos);
+                }
+            }
+            // 乱数が0以下なら左に曲がり、乱数分進む
+            if (randomAddTips < 0)
+            {
+                for (; randomAddTips < 0; randomAddTips++)
+                {
+                    xPos--;
+                    cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(xPos, 0, zPos);
+                }
+            }
         }
     }
 
