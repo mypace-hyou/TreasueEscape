df4b733088db052bf4fbf2e2766965e1b0795e8b
mypace-hyou <hyou290101@gmail.com>
2021-03-09 23:33:30 +0900

    アイテムをボックス内でランダムに配置させた

--- Assets/Script/Box/SlotGrit.cs
+++ Assets/Script/Box/SlotGrit.cs
@@ -12,39 +12,38 @@ public class SlotGrit : MonoBehaviour
 
     [SerializeField]
     private Item[] allItems;
+    private int ItemImageNum = 0;
     void Start()
     {
+        for (int j = 0; j < 11;)//どこに生成させる場所を決める
+        {
+            int ran = Random.Range(0, 28);
+            bool ch = itemHaveLiet.Contains(ran);
+            if (!ch)
+            {
+                itemHaveLiet.Add(ran);
+                j++;
+            }
+        }
         for (int i = 0; i < slotNumber; i++)
         {
             //スロット生成
             GameObject slotObj = Instantiate(slotPrefab, this.transform);
             //アイテム生成
             Slot slot = slotObj.GetComponent<Slot>();
-            for (int j = 0; j < 11;)
-            {
-                int ran = Random.Range(0, 28);
-                bool ch = itemHaveLiet.Contains(ran);
-                if (!ch)
-                {
-                    itemHaveLiet.Add(ran);
-                    j++;
-                }
-            }
+
             bool jg = itemHaveLiet.Contains(i);
-            if (jg)
+            if (jg && ItemImageNum < allItems.Length)
             {
                 //アイテムセット
-                if (i < allItems.Length)
-                {
-                    slot.SetItem(allItems[i]);
-                }
-                else
-                {
-                    slot.SetItem(null);
-                }
+                slot.SetItem(allItems[ItemImageNum]);
+                ItemImageNum++;
+            }
+            else
+            {
+                slot.SetItem(null);
             }
         }
-        //ボックス内の中身をランダムの位置に表示
         //ボックスごとに表示が違う
     }
 }
