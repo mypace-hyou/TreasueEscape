1423ab2203ed71ba01375cd84c5a673a8fc50efd
mypace-hyou <hyou290101@gmail.com>
2021-04-16 15:38:56 +0900

    任意でボックス内のアイテムをリセットできるようにした

--- Assets/Script/Box/SlotGrit.cs
+++ Assets/Script/Box/SlotGrit.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class SlotGrit : MonoBehaviour
 {
+    public GameObject slotGrit;
+    int resetnum = 0;
     [SerializeField]
     private GameObject slotPrefab;
     
     private int slotNumber = 28;//初期のボックス内に表示するスロットの数
-    List<int> itemHaveLiet = new List<int>();
+    public List<int> itemHaveList = new List<int>();
 
     [SerializeField]
     private Item[] allItems;
     private int ItemImageNum = 0;
-    //GameObject slotObj;
-    //Slot slot;
+    [SerializeField] GameObject slotObj;
+    [SerializeField] Slot slot;
 
     public List<int> BoxNumberList = new List<int>();
-    List<Item> ImageList = new List<Item>();
     [SerializeField] GameObject player;
     PlayerControl pc;
     public bool HitBox = false;
 
-    void Awake()
-    {
-        pc = player.GetComponent<PlayerControl>();
-    }
+    private int whatNumber;
+    bool test = false;
     void Start()
     {
-        for (int i = -1; i < 11;)//ボックス事に表示する場所を決める
+        pc = player.GetComponent<PlayerControl>();
+        for (int i = 0; i < 11;)//ボックス事に表示する場所を決める
         {
-            int ran = i;
+            int ran = Random.Range(1, 29);
             bool ch = BoxNumberList.Contains(ran);
             if (!ch)
             {
@@ -41,80 +41,109 @@ public class SlotGrit : MonoBehaviour
         for (int i = 0; i < 11;)//どこに生成させる場所を決める
         {
             int ran = Random.Range(0, 29);
-            bool ch = itemHaveLiet.Contains(ran);
+            bool ch = itemHaveList.Contains(ran);
             if (!ch)
             {
-                itemHaveLiet.Add(ran);
+                itemHaveList.Add(ran);
                 i++;
             }
         }
         for (int i = 0; i < slotNumber; i++)
         {
             //スロット生成
-            GameObject slotObj = Instantiate(slotPrefab, this.transform);
+            slotObj = Instantiate(slotPrefab, this.transform);
+            Debug.Log("スロット作成");
             //アイテム生成
-            Slot slot = slotObj.GetComponent<Slot>();
+            slot = slotObj.GetComponent<Slot>();
 
-            bool jg = itemHaveLiet.Contains(i);
-            if (jg && ItemImageNum < allItems.Length)
+            bool jg = itemHaveList.Contains(i);
+            if (jg)
             {
-                //アイテムセット
-                slot.SetItem(allItems[ItemImageNum]);
-                ImageList.Add(allItems[ItemImageNum]);
-                ItemImageNum++;
-            }
-            else
-            {
-                slot.SetItem(null);
-                ImageList.Add(null);
-            }
-        }
-        //ボックスごとに表示が違う
-    }
-    void Update()
-    {
-        //if (HitBox == true)
-        //{
-        //    BoxInventory();
-        //}
-    }
-    public 
-    void BoxInventory()
-    {
-        //ボックスの番号が一致したら特定のアイテムだけを表示させる
-        //ボックスの番号が一致しなかったらnullを返す
-        bool ch = BoxNumberList.Contains(pc.te);
-        if (ch)
-        {
-            for (int i = 0; i < slotNumber; i++)
-            {
-                //アイテム生成
-                Slot slot = FindObjectOfType<Slot>();
-                bool jg = itemHaveLiet.Contains(i);
-                if (jg && ItemImageNum < allItems.Length && ItemImageNum == pc.te)
+                int num = itemHaveList.IndexOf(i);
+                ItemImageNum = num;
+
+                if (whatNumber < allItems.Length)
                 {
+                    Debug.Log("アイテムセット");
                     //アイテムセット
                     slot.SetItem(allItems[ItemImageNum]);
+                    whatNumber++;
                 }
                 else
                 {
                     slot.SetItem(null);
-                    ItemImageNum++;
                 }
             }
-            HitBox = false;
         }
-        else
-        {
-            for (int i = 0; i < slotNumber; i++)
-            {
-                //アイテム生成
-                Slot slot = FindObjectOfType<Slot>();
-                slot.SetItem(null);
-            }
-            HitBox = false;
-        }
-        
     }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            test = true;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            test = false;
+        }
+        if (test)
+        {
+            testChildren();
+        }
+    }
+    [SerializeField] private GameObject parentObject;
 
+    // Use this for initialization
+    void testChildren()
+    {
+        switch (resetnum)
+        {
+            case 0://子オブジェクトを消す
+                foreach (Transform childTransform in parentObject.transform)
+                {
+                    Destroy(childTransform.gameObject);
+                }
+                whatNumber = 0;
+                Debug.Log(resetnum);
+                resetnum = 1;
+                break;
+            case 1://子オブジェクト再生成
+                for (int i = 0; i < slotNumber; i++)
+                {
+                    Debug.Log(resetnum);
+                    //スロット生成
+                    slotObj = Instantiate(slotPrefab, this.transform);
+                    //アイテム生成
+                    slot = slotObj.GetComponent<Slot>();
+
+                    bool jg = itemHaveList.Contains(i);
+                    if (jg)
+                    {
+                        int num = itemHaveList.IndexOf(i);
+                        ItemImageNum = num;
+
+                        if (whatNumber < allItems.Length)
+                        {
+                            //アイテムセット
+                            slot.SetItem(allItems[ItemImageNum]);
+                            whatNumber++;
+                        }
+                        else
+                        {
+                            slot.SetItem(null);
+                        }
+                    }
+                    if (i < slotNumber && whatNumber < allItems.Length)
+                    {
+                        resetnum = 2;
+                    }
+                }
+                break;
+            case 2://待機
+                Debug.Log(resetnum);
+                break;
+            default:
+                break;
+        }
+    }
 }
