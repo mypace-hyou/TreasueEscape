7e2e9f46642ad2287990d0ac75a4f8ac7d4ad067
mypace-hyou <hyou290101@gmail.com>
2021-06-30 13:25:08 +0900

    タイトルを作成した

---
+++ Assets/Script/Participation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Participation : MonoBehaviour
+{
+    public void LoadScen()
+    {
+        SceneManager.LoadScene("ExperimentScene");
+    }
+    public void End()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Script/Titol.cs
+++ Assets/Script/Titol.cs
@@ -2,22 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Titol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("0は何もしてない,1はエンターが当たっている")]
+    [SerializeField]bool OpenLetterCH = false;
+    [SerializeField] GameObject TitolObj;
+    [SerializeField] GameObject textLook;
+    /// <summary>EvevtTrigerのスクリプト入れる</summary>
+    public Sprite[] Sprit_UI;
+    public void event_Enter()
     {
-        
+        GetComponent<Image>().sprite = Sprit_UI[1];
+        textLook.SetActive(true);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void event_Exit()
     {
-        
+        if (OpenLetterCH == false)
+        {
+            GetComponent<Image>().sprite = Sprit_UI[0];
+            textLook.SetActive(false);
+        }
+    }
+    public void openLetter()
+    {
+        OpenLetterCH = true;
+        TitolObj.SetActive(true);
+    }
+    public void CloseLetter()
+    {
+        OpenLetterCH = false;
+        TitolObj.SetActive(false);
     }
     public void LoadScen()
     {
-        SceneManager.LoadScene("ExperimentScene");
+        if (Input.GetMouseButtonDown(0) && OpenLetterCH == true)
+        {
+            SceneManager.LoadScene("ExperimentScene");
+        }
     }
 }
